44c76c619ca5d3cbae6ef3e1e5ad41427b5ca3ae
nakamurakko <28667444+nakamurakko@users.noreply.github.com>
2023-06-18 19:02:11 +0900

    起動直後は挨拶コンポーネントを表示するように修正。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title: string = 'angular-electron-sqlite-sample';
 
-  public constructor() {
+  public constructor(
+    private router: Router
+  ) {
+  }
+
+  public ngOnInit(): void {
+    void this.router.navigate(['greeting']);
   }
 
 }
